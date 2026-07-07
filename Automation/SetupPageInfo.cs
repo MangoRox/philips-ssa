56da922fa16e7ef9d854b9b59d67712060c28c32
MangoRox <46385175+MangoRox@users.noreply.github.com>
2026-07-07 09:29:44 -0400

    class for processing system setup page info

---
+++ Automation/SetupPageInfo.cs
@@ -0,0 +1,35 @@
+using FlaUI.Core.AutomationElements;
+
+namespace SystemSetupAutomation.Automation
+{
+    internal sealed class SetupPageInfo
+    {
+        public Window Window { get; }
+        public SetupPageInfo(Window window)
+        {
+            if (window is null)
+            {
+                throw new ArgumentNullException(nameof(window), "Window cannot be null.");
+            }
+            Window = window;
+        }
+
+        public string CurrentSetupPageName()
+        {
+            var pageNameElement = Window
+                .FindFirstDescendant(cf =>
+                    cf.ByAutomationId("_bannerControl").And(cf.ByControlType(FlaUI.Core.Definitions.ControlType.Text)))
+                ?.FindFirstDescendant(cf =>
+                    cf.ByAutomationId("_lblMessage").And(cf.ByControlType(FlaUI.Core.Definitions.ControlType.Text)))
+                ?.AsLabel();
+
+            if (pageNameElement is null)
+            {
+                Console.WriteLine("ERROR: could not find page name label on setup page.");
+                return string.Empty;
+            }
+
+            return pageNameElement.Name;
+        }
+    }
+}
